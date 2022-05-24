556ad7f9e98ea80b39b18a71433d66c6f1348ced
shockolavawave <atharv.sambarat@gmail.com>
2022-05-24 19:02:22 +0530

    ; major additions ; updating .gitignore

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from Bio import Seq, SeqIO
 
 
+def print_seq(in_file):
+
+    no_seq = len(in_file)
+    print("there are", no_seq, "sequence(s) in the file\n\n")
+
+    tb_printed = ""
+
+    # primary loop for extracting all possible fasta sequences
+    for ea in in_file:
+
+        # following are operations made on each
+        print(ea.id, "          len:", no_seq)
+        print(ea.seq, "\n")
+
+
+    return tb_printed
+
+
 def main():
-    # getting the file
-    in_file = list(SeqIO.parse("files/sequence.fasta", "fasta"))
+    try:
+        # getting the file
+        in_file = list(SeqIO.parse("files/sequence.fasta", "fasta"))
 
-    # extracting sequence
-    seq1 = in_file[0].seq
-    print(seq1)
+        print_seq(in_file)
+
+        #var = in_file[0].seq
+
+    except Exception as e:
+        print("something went wrong:", e)
 
 
+# calling main
 main()
