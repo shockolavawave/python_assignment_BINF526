1a617083411dd19342edd5be8a05b0edc3083fe7
shockolavawave <atharv.sambarat@gmail.com>
2022-05-24 20:58:03 +0530

    ; major changes

--- main.py
+++ main.py
@@ -1,32 +1,85 @@
-from Bio import Seq, SeqIO
+from Bio import SeqIO
+
+
+def fasta70(seq):
+    out_seq = ""
+    try:
+        for i in range(len(seq)):
+            if i % 70 == 0 and i != 0:
+                out_seq = out_seq + "\n"
+
+            out_seq = out_seq + seq[i]
+
+    except Exception as e:
+        print("something went wrong", e)
+
+    return out_seq
+
+
+def is_dna(ea):
+    for i in ea:
+        if i == "A" or i == "G" or i == "T" or i == "C":
+            pass
+        else:
+            return False
+
+    return True
+
+
+def is_rna(ea):
+    for i in ea:
+        if i == "A" or i == "G" or i == "U" or i == "C":
+            pass
+        else:
+            return False
+
+    return True
+
+
+def is_protein(ea):
+    for i in ea:
+        if (i == "A" or i == "C" or i == "D" or i == "E" or i == "F" or i == "G" or i == "H" or
+                i == "I" or i == "K" or i == "L" or i == "M" or i == "N" or i == "E" or i == "O" or
+                i == "P" or i == "Q" or i == "R" or i == "S" or i == "T" or i == "U" or i == "V" or
+                i == "W" or i == "Y" or i == "-"):
+            pass
+        else:
+            return False
+
+    return True
+
+
+def get_seq_type(ea):
+    if is_dna(ea):
+        return "DNA"
+    elif is_rna(ea):
+        return "RNA"
+    elif is_protein(ea):
+        return "protein"
+    else:
+        return "unidentified"
 
 
 def print_seq(in_file):
-
-    no_seq = len(in_file)
-    print("there are", no_seq, "sequence(s) in the file\n\n")
-
-    tb_printed = ""
+    print("there are", len(in_file), "sequence(s) in the file\n\n")
 
     # primary loop for extracting all possible fasta sequences
     for ea in in_file:
-
         # following are operations made on each
-        print(ea.id, "          len:", no_seq)
-        print(ea.seq, "\n")
+        ea.seq = ea.seq.upper()  # setting the sequence to upper case
 
-
-    return tb_printed
+        print(ea.id, "          len:", len(ea), "       type:", get_seq_type(ea))
+        print(fasta70(ea.seq), "\n\n")
 
 
 def main():
     try:
         # getting the file
-        in_file = list(SeqIO.parse("files/sequence.fasta", "fasta"))
+        in_file = list(SeqIO.parse("files/var.fasta", "fasta"))
 
         print_seq(in_file)
 
-        #var = in_file[0].seq
+        # var = in_file[0].seq
 
     except Exception as e:
         print("something went wrong:", e)
